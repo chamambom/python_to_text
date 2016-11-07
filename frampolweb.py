2a5a3c1a407077039175a38323c599b62b3efee9
mchamambo <martin@teamzol.co.zw>
2016-11-07 11:58:40 +0200

    added a requirements.txt

--- frampolweb.py
+++ frampolweb.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-from flask import Flask, render_template, request, redirect, url_for, flash, session, abort, g, Response, json ,make_response
+from flask import Flask, render_template, request, redirect, url_for, flash, session, abort, g, Response, json
 import MySQLdb
 import databaseconfig
 import os
